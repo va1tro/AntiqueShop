3350e353eefecc411ea58558d930fb2f66a12c71
va1tro <rud2lentina@gmail.com>
2025-05-26 22:21:54 +0300

    закончить adminPage

--- AntiqueShop/Pages/ItemDetailsPage.xaml.cs
+++ AntiqueShop/Pages/ItemDetailsPage.xaml.cs
@@ -23,27 +23,7 @@ namespace AntiqueShop.Pages
         public ItemDetailsPage(Items selectedItem)
         {
             InitializeComponent();
-
-            ItemName.Text = selectedItem.name_item;
-            ItemYear.Text = selectedItem.year.ToString();
-            ItemCondition.Text = selectedItem.condition;
-            ItemAuthenticity.Text = selectedItem.authenticity;
-            ItemPurchasePrice.Text = $"{selectedItem.purchase_price} руб.";
-            ItemSellingPrice.Text = $"{selectedItem.selling_price} руб.";
-            ItemArrivalDate.Text = selectedItem.arrival_date?.ToShortDateString();
-
-            if (selectedItem.image != null && selectedItem.image.Length > 0)
-            {
-                using (ItemImage.Source = new BitmapImage(new Uri(selectedItem.image, UriKind.RelativeOrAbsolute)));
-                {
-                    var bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmap.StreamSource = stream;
-                    bitmap.EndInit();
-                    ItemImage.Source = bitmap;
-                }
-            }
+            this.DataContext = selectedItem;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
